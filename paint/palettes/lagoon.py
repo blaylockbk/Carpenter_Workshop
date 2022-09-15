54e53cd22a8502f24b0018c82457317711d6e7fb
Brian Blaylock <blaylockbk@users.noreply.github.com>
2022-09-15 12:13:07 -0700

    added paint for Lagoon.

---
+++ paint/palettes/lagoon.py
@@ -0,0 +1,14 @@
+## Brian Blaylock
+## September 15, 2022
+
+"""
+Color Palettes inspired by rides at Lagoon
+"""
+
+class colossus():
+    colors = dict(
+        blue="#0011ff"
+        gold="#ffd000"
+        orange="#f8501c"
+        cream="#ffe4bc"
+    )
