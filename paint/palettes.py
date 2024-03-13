bfd25cf2c08b3aad25b225d401fbc09438deae66
Brian Blaylock <blaylockbk@users.noreply.github.com>
2024-03-13 09:06:11 -0700

    add gnome pallette

--- paint/palettes.py
+++ paint/palettes.py
@@ -100,3 +100,59 @@ sesameStreet = dict(
     zoe="#FDB947",
     rubberDucky="#FEFF54",
 )
+
+# GNOME Project
+# https://developer.gnome.org/hig/reference/palette.html
+gnome = {
+    "Blue 1": "#99c1f1",
+"Blue 2": "#62a0ea",
+"Blue 3": "#3584e4",
+"Blue 4": "#1c71d8",
+"Blue 5": "#1a5fb4",
+"Green 1": "#8ff0a4",
+"Green 2": "#57e389",
+"Green 3": "#33d17a",
+"Green 4": "#2ec27e",
+"Green 5": "#26a269",
+"Yellow 1": "#f9f06b",
+"Yellow 2": "#f8e45c",
+"Yellow 3": "#f6d32d",
+"Yellow 4": "#f5c211",
+"Yellow 5": "#e5a50a",
+"Orange 1": "#ffbe6f",
+"Orange 2": "#ffa348",
+"Orange 3": "#ff7800",
+"Orange 4": "#e66100",
+"Orange 5": "#c64600",
+"Red 1": "#f66151",
+"Red 2": "#ed333b",
+"Red 3": "#e01b24",
+"Red 4": "#c01c28",
+"Red 5": "#a51d2d",
+"Purple 1": "#dc8add",
+"Purple 2": "#c061cb",
+"Purple 3": "#9141ac",
+"Purple 4": "#813d9c",
+"Purple 5": "#613583",
+"Brown 1": "#cdab8f",
+"Brown 2": "#b5835a",
+"Brown 3": "#986a44",
+"Brown 4": "#865e3c",
+"Brown 5": "#63452c",
+"Light 1": "#ffffff",
+"Light 2": "#f6f5f4",
+"Light 3": "#deddda",
+"Light 4": "#c0bfbc",
+"Light 5": "#9a9996",
+"Dark 1": "#77767b",
+"Dark 2": "#5e5c64",
+"Dark 3": "#3d3846",
+"Dark 4": "#241f31",
+"Dark 5": "#000000"
+}
+
+gnome1 = {k:v for k, v in gnome.items() if k.endswith('1')}
+gnome2 = {k:v for k, v in gnome.items() if k.endswith('2')}
+gnome3 = {k:v for k, v in gnome.items() if k.endswith('3')}
+gnome4 = {k:v for k, v in gnome.items() if k.endswith('4')}
+gnome5 = {k:v for k, v in gnome.items() if k.endswith('5')}
